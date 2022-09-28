7eb4e0249af561265bfc437712f17ef688f32ae9
Juan Robles <juanestebanrobles3103@gmail.com>
2022-09-28 17:44:38 -0500

    Se hace merged a la vista de clientes

--- resources/views/Cliente/index.blade.php
+++ resources/views/Cliente/index.blade.php
@@ -63,29 +63,6 @@
     <script>
         //Lo correcto es adjuntar mejor un JS que contenga todo, pero lo haremos así mientras xd nada más por el ejemplo
         //Si el JavaScript adjunto requiere renderizar cositas de blade, este debe guardarse como "archivo.blade.js"
-<<<<<<< Updated upstream
-
-        async function postData(url = '', data = {}) {
-            // Esta función en realidad es genérica, sirve para cualquier método que requiera hacer alguna petición al servidor
-            const response = await fetch(url, {
-                method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                mode: 'cors', // no-cors, *cors, same-origin
-                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: 'same-origin', // include, *same-origin, omit
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': csrf_token
-                    // 'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                redirect: 'follow', // manual, *follow, error
-                referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-                body: JSON.stringify(data)
-            });
-            return response.json(); // Convierte la respuesta del servidor en un objeto JSON
-        }
-
-=======
->>>>>>> Stashed changes
         function crearCliente(){
             let razon_social = document.getElementById("razon_social_crear").value;
             let identificacion = document.getElementById("identificacion_crear").value;
