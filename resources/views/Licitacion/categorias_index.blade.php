ac2edadca62cf46bca60caf8bc984de70616865f
draaclex <marlyacosta.a3@gmail.com>
2022-10-11 23:03:48 -0500

    Cambios front para vista de categorias
    Ajustes componente de cateogorias y en la vista que los implementa

--- resources/views/Licitacion/categorias_index.blade.php
+++ resources/views/Licitacion/categorias_index.blade.php
@@ -4,13 +4,18 @@
 
 @include('layouts.headers.cards')
 <div class="container-fluid mt--9">
-    <div style="width: 18rem;">
+    <div class="col justify-content-end text-right">
+        <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#">
+            Crear <i class="fas fa-plus"></i>
+        </button>
+    </div>
+    <div class="row align-items-center">
         @foreach ($categorias as $cat)
-            <x-categoria-element
-            componentId="{{$cat->id}}"
-            componentTitle="{{$cat->nombre}}"
-            :modelo="$cat"
-            />
+        <div class="col pt-4">
+            <div style="width: 18rem;">
+                <x-categoria-element componentId="{{$cat->id}}" componentTitle="{{$cat->nombre}}" :modelo="$cat" />
+            </div>
+        </div>
         @endforeach
     </div>
 </div>
--- resources/views/components/categoria-element.blade.php
+++ resources/views/components/categoria-element.blade.php
@@ -1,14 +1,18 @@
 <div class="card card-stats mb-4 mb-lg-0">
     <div class="card-body">
         <div class="row">
-            <div class="col">
-                <a href="#" class="btn btn-sm text-default" onclick="" title="Acciones" data-toggle="tooltip" data-placement="bottom">
-                    <i class="fas fa-bars"></i>
-                </a>
-            </div>
             <div class="col" style="position: relative; display: inline-block; text-align: center;">
+                <div class="dropdown" style="position: absolute; top: 0px; left: 0px;">
+                    <a class="btn btn-sm text-default" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-bars"></i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-left dropdown-menu-arrow">
+                        <a class="dropdown-item" href="#">Editar</a>
+                        <a class="dropdown-item" href="#">Eliminar</a>
+                    </div>
+                </div>
                 <i class="fas fa-folder fa-10x fa-lg" style="color:{{$modelo->css_style}}"></i> <!-- hacer que el color sea aleatorio-->
-                <div class="text-muted text-sm" style="position: absolute; top: 5px; left: 35px;">{{$modelo->nombre}}</div>
+                <div class="text-muted text-sm" style="position: absolute; top: 5px; left: 70px;">{{$modelo->nombre}}</div>
             </div>
 
         </div>
@@ -28,7 +32,7 @@
 </div>
 
 <script>
-    function testAllGood(){
+    function testAllGood() {
         console.log('XD');
     }
 </script>
