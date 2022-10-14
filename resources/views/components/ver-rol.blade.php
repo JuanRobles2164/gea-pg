671d8a4407455477cde92065a9d3bf020c91347f
Gabriel Alexander Castro Vargas <55069295+gabocastro29@users.noreply.github.com>
2022-10-13 20:17:39 -0500

    Cambio modals
    se realizaron cambios visuales a todos los modals hasta la fecha

--- resources/views/components/ver-rol.blade.php
+++ resources/views/components/ver-rol.blade.php
@@ -1,21 +1,27 @@
-
 @extends('templates.templateModalVisualizar')
 
-@section('modal-visualizer')    
+@section('modal-visualizer')
 
 <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
-
-    <div class="form-group">
-        <label class="form-label" for="nombre_rol_modal_details_id">Nombre:</label>
-        <br>
-        <label for="" class="form-input" id="nombre_rol_modal_details_id">{{isset($model->id) ? $model->nombre : '' }}</label>
+<form>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="nombre_rol_modal_details_id">Nombre:</label>
+                <input class="form-control form-control-alternative" id="nombre_rol_modal_details_id">{{isset($model->id) ? $model->nombre : '' }}</input>
+            </div>
+        </div>
     </div>
-
-    <div class="form-group">
-        <label class="form-label" for="descripcion_rol_modal_details_id">Descripcion:</label>
-        <br>
-        <textarea name="" id="descripcion_rol_modal_details_id" cols="30" rows="5" readonly>
-            {{isset($model->id) ? $model->descripcion : ''}}
-        </textarea>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="descripcion_rol_modal_details_id">Descripcion:</label>
+                <textarea class="form-control form-control-alternative" id="descripcion_rol_modal_details_id" rows="5" readonly>
+                {{isset($model->id) ? $model->descripcion : ''}}
+                </textarea>
+            </div>
+        </div>
     </div>
+</form>
+
 @endsection
--- resources/views/components/ver-tipo-documento.blade.php
+++ resources/views/components/ver-tipo-documento.blade.php
@@ -1,40 +1,51 @@
 @extends('templates.templateModalVisualizar')
 
-@section('modal-visualizer')    
+@section('modal-visualizer')
 
 <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
-<div class="form-group">
-    <label class="form-label" for="nombre_tipo_documento_modal_view_id">Nombre:</label>
-    <br>
-    <input type="text" class="form-input" id="nombre_tipo_documento_modal_view_id">
-</div>
+<form>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="nombre_tipo_documento_modal_view_id">Nombre:</label>
+                <input type="text" class="form-control form-control-alternative" id="nombre_tipo_documento_modal_view_id">
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="custom-control custom-radio mb-3">
+                <input type="radio" class="custom-control-input" name="recurrente_constante_documento_modal_view" id="constante_tipo_documento_modal_view_id">
+                <label class="custom-control-label" for="constante_tipo_documento_modal_view_id">Constante</label>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="custom-control custom-radio mb-3">
+                <input type="radio" class="custom-control-input" name="recurrente_constante_documento_modal_view" id="recurrente_tipo_documento_modal_view_id">
+                <label class="custom-control-label" for="recurrente_tipo_documento_modal_view_id">Recurrente</label>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="unidad_validez_tipo_documento_modal_view_id">Unidad Validez:</label>
+                <select class="form-control form-control-alternative" name="unidad_validez_tipo_documento_modal_view" id="unidad_validez_tipo_documento_modal_view_id">
+                    <option value="-1" disabled>Seleccione una opción...</option>
+                    <option value="1">Dias</option>
+                    <option value="2">Semanas</option>
+                    <option value="3">Meses</option>
+                    <option value="4">Años</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="validez_tipo_documento_modal_view_id">Validez:</label>
+                <input type="text" class="form-control form-control-alternative" id="validez_tipo_documento_modal_view_id">
+            </div>
+        </div>
+    </div>
+</form>
 
-<div class="form-check">
-    <input type="radio" class="form-check-input" name="recurrente_constante_documento_modal_view" id="recurrente_tipo_documento_modal_view_id">
-    <label class="form-check-label" for="recurrente_tipo_documento_modal_view_id">Recurrente</label>
-</div>
-
-<div class="form-check">
-    <input type="radio" class="form-check-input" name="recurrente_constante_documento_modal_view" id="constante_tipo_documento_modal_view_id" >
-    <label class="form-check-label" for="constante_tipo_documento_modal_view_id">Constante</label>
-</div>
-
-<div class="form-group">
-    <label class="form-label" for="validez_tipo_documento_modal_view_id">Validez:</label>
-    <br>
-    <input type="text" class="form-input" id="validez_tipo_documento_modal_view_id">
-</div>
-
-<div class="form-group">
-    <label class="form-label" for="unidad_validez_tipo_documento_modal_view_id">Unidad Validez:</label>
-    <br>
-    <select name="unidad_validez_tipo_documento_modal_view" id="unidad_validez_tipo_documento_modal_view_id">
-        <option value="-1">Seleccione una opción...</option>
-        <option value="1">Dias</option>
-        <option value="2">Semanas</option>
-        <option value="3">Meses</option>
-        <option value="4">Años</option>
-    </select>
-
-</div>
 @endsection
