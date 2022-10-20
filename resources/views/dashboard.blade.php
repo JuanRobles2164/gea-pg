74602d15e3511f94fc1c9e0a3a4236d0f7522d81
Alexandra Acosta <61189229+MarlyAcosta@users.noreply.github.com>
2022-10-19 21:03:52 -0500

    home card creation

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,12 +4,72 @@
     @include('layouts.headers.cards')
     
     <div class="container-fluid ">
-        <div class="row">
-           //para donde vaya a meter la tabla guiese de otra pantalla pero pues la variale a iterar se llama '$licitaciones'
+        <div class="row row-home">
+            <div class="column col-6">
+                <div class="card shadow card-home1">
+                    <h4>Licitaciones creadas el último mes</h4>
+                    <div class="table-responsive">
+                        <table class="table align-items-center">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Fecha Inicio</th>
+                                    <th scope="col">Fecha Fin</th>
+                                    <th scope="col">Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($licitaciones as $li)
+                                <tr>
+                                    <td scope="row">{{$li->nombre}}</td>
+                                    <td scope="row">{{$li->fecha_inicio}}</td>
+                                    <td scope="row">{{$li->fecha_fin}}</td>
+                                    <td scope="row">
+                                        <a href="#" class="btn btn-info btn-sm" onclick="setDataToClienteModal({{$li->id}})" title="Ver" data-toggle="tooltip" data-placement="bottom">
+                                            <i class="fas fa-eye"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="column col-6">
+                <div class="card shadow card-home2">
+                    <h4>Licitaciones proximas a vencer</h4>
+                    <div class="table-responsive">
+                        <table class="table align-items-center">
+                            <thead class="thead-light">
+                                <tr>
+                                    <td scope="row">{{$li->nombre}}</td>
+                                    <td scope="row">{{$li->fecha_inicio}}</td>
+                                    <td scope="row">{{$li->fecha_fin}}</td>
+                                    <th scope="col">Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($licitaciones as $li)
+                                <tr>
+                                    <td scope="row">{{$li->id}}</td>
+                                    <td scope="row">{{$li->nombre}}</td>
+                                    <td scope="row">{{$li->descripcion}}</td>
+                                    <td scope="row">{{$li->fecha_inicio}}</td>
+                                    <td scope="row">
+                                        <a href="#" class="btn btn-info btn-sm" onclick="setDataToClienteModal({{$li->id}})" title="Ver" data-toggle="tooltip" data-placement="bottom">
+                                            <i class="fas fa-eye"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>    
         </div>
-        <div class="row mt-5">
-           
-        </div>
+            
         
         @include('layouts.footers.auth')
     </div>
