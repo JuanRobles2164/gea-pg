7c0489202447f5a6d3881cc059797f7943c1a8bb
draaclex <marlyacosta.a3@gmail.com>
2022-10-31 20:28:52 -0500

    Funcionalidad edición parametros basicos licitacion

--- resources/views/components/guardar-licitacion.blade.php
+++ resources/views/components/guardar-licitacion.blade.php
@@ -1,43 +1,67 @@
-@extends('templates.templateComponents')
+@extends('templates.templateComponentes')
 
 @section('modal-content')
-<form>
+<form class="modal-lg">
     <input type="hidden" name="" id="id_licitacion_crear_modal_id">
-    <div class="form-group">
-        <label for="">Número:</label>
-        <input type="text" name="" id="numero_licitacion_crear_modal_id" autocomplete="disabled">
+    <div class="row align-items-center">
+        <div class="col-md-4">
+            <div class="form-group">
+                <label for="numero_licitacion_crear_modal_id">Número:</label>
+                <input type="text" class="form-control form-control-alternative" name="" id="numero_licitacion_crear_modal_id" autocomplete="disabled" readonly>
+            </div>
+        </div>
+        <div class="col">
+            <div class="form-group">
+             <label for="categoria_licitacion_crear_modal_id">Categoría:</label>
+                <select name="categoria_licitacion_crear" class="custom-select form-control-alternative" id="categoria_licitacion_crear_modal_id">
+                    <option value="0">Seleccione una categoria...</option>
+                        @foreach ($categorias as $c)
+                            <option value="{{ $c->id }}">{{ $c->nombre }}</option>
+                        @endforeach
+                </select>
+            </div>
+        </div>
     </div>
-
     <div class="form-group">
-        <label for="nombre_licitacion_crear_modal_id">Nombre</label>
-        <input type="text" name="nombre_licitacion_crear" id="nombre_licitacion_crear_modal_id" autocomplete="disabled">
+        <label for="nombre_licitacion_crear_modal_id">Nombre:</label>
+        <input type="text" class="form-control form-control-alternative" name="nombre_licitacion_crear" id="nombre_licitacion_crear_modal_id" autocomplete="disabled">
     </div>
 
     <div class="form-group">
         <label for="descripcion_licitacion_crear_modal_id">Descripción:</label>
-        <textarea name="descripcion_licitacion_crear" id="descripcion_licitacion_crear_modal_id" cols="30" rows="10" autocomplete="disabled">
+        <textarea name="descripcion_licitacion_crear" class="form-control form-control-alternative" id="descripcion_licitacion_crear_modal_id" cols="30" rows="5" autocomplete="disabled">
             
         </textarea>
     </div>
-
-    <div class="form-group">
-        <label for="fecha_inicio_licitacion_crear_modal_id">Fecha de inicio:</label>
-        <input type="date" name="fecha_inicio_licitacion_crear" id="fecha_inicio_licitacion_crear_modal_id" autocomplete="disabled">
-    </div>
-
-    <div class="form-group">
-        <label for="fecha_fin_licitacion_crear_modal_id">Fecha de fin:</label>
-        <input type="date" name="fecha_fin_licitacion_crear" id="fecha_fin_licitacion_crear_modal_id" autocomplete="disabled">
-    </div>
-
-    <div class="form-group">
-        <label for="clonado_licitacion_crear_modal_id">Clonado</label>
-        <input type="checkbox" name="clonado_licitacion_crear" id="clonado_licitacion_crear_modal_id">
+    <div class="input-daterange datepicker row align-items-center">
+        <div class="col">
+            <div class="form-group">
+                <label for="fecha_inicio_licitacion_crear_modal_id">Fecha Inicio:</label>
+                <div class="input-group input-group-alternative">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="ni ni-calendar-grid-58"></i></span>
+                    </div>
+                    <input class="form-control form-control-alternative datepicker" id="fecha_inicio_licitacion_crear_modal_id" autocomplete="disabled" placeholder="Fecha de inicio:" type="text" name="fecha_inicio">
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="form-group">
+                <label for="fecha_fin_licitacion_crear_modal_id">Fecha Fin:</label>
+                <div class="input-group input-group-alternative">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="ni ni-calendar-grid-58"></i></span>
+                    </div>
+                    <input id="fecha_fin_licitacion_crear_modal_id" autocomplete="disabled" class="form-control form-control-alternative datepicker" placeholder="Fecha Finalización" type="text" name="fecha_fin">
+                </div>
+            </div>
+        </div>
     </div>
 
     <div class="form-group">
         <label for="cliente_licitacion_crear_modal_id">Seleccione el Cliente:</label>
-        <select name="cliente_licitacion_crear" id="cliente_licitacion_crear_modal_id">
+        <select name="cliente_licitacion_crear" class="custom-select form-control-alternative" id="cliente_licitacion_crear_modal_id">
+            <option value="0">Seleccione un cliente...</option>
             @foreach ($clientes as $c)
                 <option value="{{ $c->id }}">{{ $c->razon_social }}</option>
             @endforeach
@@ -45,22 +69,9 @@
     </div>
 
     <div class="form-group">
-        <label for="estado_licitacion_crear_modal_id">Estado:</label>
-        <input type="text" name="estado_licitacion_crear" id="estado_licitacion_crear_modal_id">
-    </div>
-
-    <div class="form-group">
-        <label for="categoria_licitacion_crear_modal_id">Categoría:</label>
-        <select name="categoria_licitacion_crear" id="categoria_licitacion_crear_modal_id">
-            @foreach ($categorias as $c)
-                <option value="{{ $c->id }}">{{ $c->nombre }}</option>
-            @endforeach
-        </select>
-    </div>
-
-    <div class="form-group">
-        <label for="tipo_licitacion_licitacion_crear_modal_id">Seleccione tipo de licitación:</label>
-        <select name="tipo_licitacion_licitacion_crear" id="tipo_licitacion_licitacion_crear_modal_id">
+        <label for="tipo_licitacion_licitacion_crear_modal_id">Seleccione el Tipo de licitación:</label>
+        <select name="tipo_licitacion_licitacion_crear" class="custom-select form-control-alternative" id="tipo_licitacion_licitacion_crear_modal_id">
+            <option value="0">Seleccione un tipo de licitacion...</option>
             @foreach ($tipos_licitacion as $tl)
                 <option value="{{ $tl->id }}">{{ $tl->nombre }}</option>
             @endforeach
@@ -72,34 +83,39 @@
 
 @section('scripts-modal')
     <script>
-        {{$modal_id}}Crear(){
+        function {{$modal_id}}Crear(){
+            let id = document.getElementById("id_licitacion_crear_modal_id").value;
             let numero = document.getElementById("numero_licitacion_crear_modal_id").value;
             let nombre = document.getElementById("nombre_licitacion_crear_modal_id").value;
             let descripcion = document.getElementById("descripcion_licitacion_crear_modal_id").value;
             let fecha_inicio = document.getElementById("fecha_inicio_licitacion_crear_modal_id").value;
             let fecha_fin = document.getElementById("fecha_fin_licitacion_crear_modal_id").value;
-            let clonado = document.getElementById("clonado_licitacion_crear_modal_id").value;
             let cliente = document.getElementById("cliente_licitacion_crear_modal_id").value;
-            let estado = document.getElementById("estado_licitacion_crear_modal_id").value;
             let categoria = document.getElementById("categoria_licitacion_crear_modal_id").value;
             let tipo_licitacion = document.getElementById("tipo_licitacion_licitacion_crear_modal_id").value;
 
             let data = {
+                id: id,
                 numero: numero,
                 nombre: nombre,
                 descripcion: descripcion,
                 fecha_inicio: fecha_inicio,
                 fecha_fin: fecha_fin,
-                clonado: clonado,
                 cliente: cliente,
-                estado: estado,
                 categoria: categoria,
                 tipo_licitacion: tipo_licitacion
             }
-            postData('{{route("licitacion.guardar")}}', data)
+            postData('{{route("licitacion.actualizar")}}', data)
             .then((data) => {
                 console.log(data);
-                alert("Licitación creada exitosamente!");
+                    swal({
+                        title: "Licitacion editada exitosamente!",
+                        icon: "success",
+                        buttons: "OK",
+                    })
+                    .then((willDelete) => {
+                        location.reload();
+                    });
             });
         }
     </script>
