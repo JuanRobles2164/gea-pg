b9bbcf9143e49b4fd3805ff11c2b33dd027ccd0b
Juan Robles <juanestebanrobles3103@gmail.com>
2023-03-04 18:09:10 -0500

    Actualización de dependencias
    Se reinstala el proyecto y se actualizan las dependencias, para evitar problemas de seguridad

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit83be5fd725945ba68ea7278714f13412
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit83be5fd725945ba68ea7278714f13412::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire83be5fd725945ba68ea7278714f13412($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit83be5fd725945ba68ea7278714f13412::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire83be5fd725945ba68ea7278714f13412($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
